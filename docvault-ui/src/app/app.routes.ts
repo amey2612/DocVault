03dc925d882aebde1f21c41346ab9cfb5635b5d6
Amey Anil Khot <84377278+amey2612@users.noreply.github.com>
2026-02-17 16:35:16 +0530

    Delete docvault-ui directory

--- docvault-ui/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Upload } from './upload/upload';
-import { DocumentList } from './document-list/document-list';
-
-export const routes: Routes = [
-  { path: '', component: Upload },
-  { path: 'documents', component: DocumentList}
-];
